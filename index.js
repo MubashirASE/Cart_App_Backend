2f31ac21095836369a60fd64a8e78da789d7d8f0
MubashirASE <mubashir.sohail@codefinitysol.com>
2026-01-06 17:45:33 +0500

    error handling in this

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import connectDB from "./database/db.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import userRoutes from "./routes/user.route.js";
-import adminRoutes from "./routes/admin.route.js";
 import orderRoutes from "./routes/order.route.js";
 import categoryRoutes from "./routes/category.route.js";
 
